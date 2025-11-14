b94d5ec584736ae57045dab1fab7addc1ae0a723
Phoenix Bastien <91162508+PhoenixBastien@users.noreply.github.com>
2025-11-14 10:54:41 -0500

    Update hummingbird.ps1

--- hummingbird.ps1
+++ hummingbird.ps1
@@ -2,98 +2,93 @@ using namespace Hummingbird.DM.Server.Interop.PCDClient
 
 Add-Type -Path "C:\Program Files (x86)\Open Text\DM Extensions\Hummingbird.DM.Server.Interop.PCDClient.dll"
 
-# get document numbers from input file
-$inputFilePath = ".\in\RDIMIS Migrated Documents.csv"
-$toDelete = $(Get-Content $inputFilePath) -split ","
-
-# remove previous log file if it exists
+# input and log paths
+$inputFilePath = ".\in\Copy of RDIMIS Migrated Documents.csv"
 $logFilePath = ".\errors.log"
-if (Test-Path $logFilePath) {
-    Remove-Item $logFilePath
-}
-
-# create credential file if it doesn't exist
 $credFilePath = ".\cred.xml"
+
+# clean up old log
+if (Test-Path $logFilePath) { Remove-Item $logFilePath }
+
+# credentials
 if (!(Test-Path $credFilePath)) {
     Get-Credential -Credential $Env:USERNAME | Export-Clixml $credFilePath
 }
 $cred = Import-Clixml $credFilePath
 
-# log into edocs account and get dst
+# login
 $lib = "PS-SP"
 $login = New-Object PCDLoginClass
-$login.AddLogin(0, $lib, $cred.UserName, $cred.GetNetworkCredential().Password) +
+$login.AddLogin(0, $lib, $cred.UserName, $cred.GetNetworkCredential().Password) | Out-Null
 $login.Execute() | Out-Null
 $dst = $login.GetDST()
 
 # create search object to find document numbers
 $search = New-Object PCDSearchClass
-$search.SetDST($dst) +
-$search.AddSearchLib($lib) +
-$search.SetSearchObject("PS-SP_PROF_ML_2") +
+$search.SetDST($dst) | Out-Null
+$search.AddSearchLib($lib) | Out-Null
+$search.SetSearchObject("PS-SP_PROF_ML_2") | Out-Null
 $search.AddReturnProperty("DOCNUM") | Out-Null
+
+# reusable SQL object
+$sql = New-Object PCDSQLClass
+$sql.SetDST($dst) | Out-Null
+$sql.SetLibrary($lib) | Out-Null
 
 # chunk size of document numbers to search at once
 $chunkSize = 10000
+
+# get document numbers from input file
+$toDelete = (Get-Content $inputFilePath -Raw).Trim() -split "[,\r\n]+"
 
 for ($i = 0; $i -lt $toDelete.Count; $i += $chunkSize) {
     # split array into chunks of document numbers
     Write-Host "Searching for documents $($i + 1) to $($i + $chunkSize)..."
     $chunk = $toDelete[$i..($i + $chunkSize - 1)]
+    $docNums = $chunk -join ","
 
     # search for chunk of document numbers
-    $search.AddSearchCriteria("DOCNUM", $chunk -join ",") +
+    $search.AddSearchCriteria("DOCNUM", $docNums) | Out-Null
     $search.Execute() | Out-Null
 
     # get number of documents found
     $count = $search.GetRowsFound()
     Write-Host "$count documents found"
+    
+    # batch update READONLY for docs
+    $sql.Execute("UPDATE docsadm.profile SET readonly = 'N' WHERE docnumber IN ($docNums)") | Out-Null
+
+    # batch update READONLY for parent docs
+    $sql.Execute("UPDATE docsadm.profile SET readonly = 'N' WHERE docnumber IN (SELECT parent FROM docsadm.folder_item WHERE docnumber IN ($docNums))") | Out-Null
+
+    # find document link ids
+    $sql.Execute("SELECT system_id FROM docsadm.folder_item WHERE docnumber IN ($docNums)") | Out-Null
+
+    do {
+        # delete document link
+        $linkId = $sql.GetColumnValue(1)
+        $link = New-Object PCDDocObjectClass
+        $link.SetDST($dst) | Out-Null
+        $link.SetObjectType("ContentItem") | Out-Null
+        $link.SetProperty("%TARGET_LIBRARY", $lib) | Out-Null
+        $link.SetProperty("SYSTEM_ID", $linkId) | Out-Null
+        $link.Delete() | Out-Null
+    } while ($sql.NextRow())
+
+    # release results
+    $sql.ReleaseResults() | Out-Null
 
     # begin get block
     $search.BeginGetBlock() | Out-Null
 
     while ($search.NextRow()) {
-        # get document by document number and remove read-only
+        # delete document
         $docNum = $search.GetPropertyValue("DOCNUM")
         $doc = New-Object PCDDocObjectClass
-        $doc.SetDST($dst) +
-        $doc.SetObjectType("PS-SP_PROF_ML_2") +
-        $doc.SetProperty("%TARGET_LIBRARY", $lib) +
-        $doc.SetProperty("%OBJECT_IDENTIFIER", $docNum) +
-        $doc.SetProperty("READONLY", "N") +
-        $doc.Update() | Out-Null
-
-        # find document link ids and parent document numbers
-        $sql = New-Object PCDSQLClass
-        $sql.SetDST($dst) +
-        $sql.SetLibrary($lib) +
-        $sql.Execute("select parent, system_id from docsadm.folder_item where docnumber = $docNum") | Out-Null
-
-        for ($j = 0; $j -lt $sql.GetRowCount(); $j++) {
-            $parentDocNum = $sql.GetColumnValue(1)
-            $parentDoc = New-Object PCDDocObjectClass
-            $parentDoc.SetDST($dst) +
-            $parentDoc.SetObjectType("PS-SP_PROF_ML_2") +
-            $parentDoc.SetProperty("%TARGET_LIBRARY", $lib) +
-            $parentDoc.SetProperty("%OBJECT_IDENTIFIER", $parentDocNum) +
-            $parentDoc.SetProperty("READONLY", "N") +
-            $parentDoc.Update() | Out-Null
-
-            # get document link by link id and delete document link
-            $linkId = $sql.GetColumnValue(2)
-            $link = New-Object PCDDocObjectClass
-            $link.SetDST($dst) +
-            $link.SetObjectType("ContentItem") +
-            $link.SetProperty("%TARGET_LIBRARY", $lib) +
-            $link.SetProperty("SYSTEM_ID", $linkId) +
-            $link.Delete() | Out-Null
-
-            # continue to next document link
-            $sql.NextRow() | Out-Null
-        }
-
-        # release results and delete document
-        $sql.ReleaseResults() +
+        $doc.SetDST($dst) | Out-Null
+        $doc.SetObjectType("PS-SP_PROF_ML_2") | Out-Null
+        $doc.SetProperty("%TARGET_LIBRARY", $lib) | Out-Null
+        $doc.SetProperty("%OBJECT_IDENTIFIER", $docNum) | Out-Null
         $doc.Delete() | Out-Null
 
         # write to log if error occurs
@@ -103,6 +98,6 @@ for ($i = 0; $i -lt $toDelete.Count; $i += $chunkSize) {
     }
 
     # end get block and release results
-    $search.EndGetBlock() +
+    $search.EndGetBlock() | Out-Null
     $search.ReleaseResults() | Out-Null
 }
